f8544a82f02cb2fa678bc7dc2f39186e1d53bb87
dicechick373 <dicechick373@gmail.com>
2024-01-25 19:56:57 +0900

    test

--- static/土木技術管理規程集/道路１編/02_PDFLoader_CustomMetadata.py
+++ static/土木技術管理規程集/道路１編/02_PDFLoader_CustomMetadata.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import copy
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
@@ -29,6 +30,23 @@ def pdf_loader(pdf_file: str):
     return docs
 
 
+def format_docs(org_docs, page_prefix: int):
+    '''Documentのmetadataを加工する関数'''
+
+    docs = copy.deepcopy(org_docs)
+    for doc in docs:
+        # sourceを「土木技術管理規程集_道路１編」のフォーマットに修正
+        source = doc.metadata["source"].split("/")
+        new_source = source[1] + "_" + source[2].split("\\")[0]
+        doc.metadata.update({"source": new_source})
+
+        # ページ番号を「1-1」のフォーマットに修正
+        new_page = f'{str(page_prefix)}-{str(doc.metadata["page"]+1)}'
+        doc.metadata.update({"page": new_page})
+
+    return docs
+
+
 if __name__ == "__main__":
     # PDFファイルを指定
     file = 'static/土木技術管理規程集/道路１編/第１章_道路一般.pdf'
@@ -36,11 +54,13 @@ if __name__ == "__main__":
     # Documentに変換
     docs = pdf_loader(file)
 
-    print(f'{len(docs)}個のChunkを読み込みました。')
+    format_docs = format_docs(docs, 1)
+
+    print(f'{len(format_docs)}個のChunkを読み込みました。')
     print('-------------------------------------------------------------------------')
-    print(f'page_content:{docs[0].page_content}')
+    print(f'page_content:{format_docs[0].page_content}')
     print('-------------------------------------------------------------------------')
-    print(f'source:{docs[0].metadata["source"]}')
+    print(f'source:{format_docs[0].metadata["source"]}')
     print('-------------------------------------------------------------------------')
-    print(f'page:{docs[0].metadata["page"]}')
+    print(f'page:{format_docs[0].metadata["page"]}')
     print('-------------------------------------------------------------------------')
