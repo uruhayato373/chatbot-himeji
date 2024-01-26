115bf2d34a925d4d1931c2598f00d7662b07fde4
dicechick373 <dicechick373@gmail.com>
2024-01-26 09:40:37 +0900

    test

--- static/土木技術管理規程集/砂防編_急傾斜/save_faiss.py
+++ static/土木技術管理規程集/砂防編_急傾斜/save_faiss.py
@@ -1,4 +1,3 @@
-import os
 import glob
 import streamlit as st
 import copy
@@ -18,19 +17,20 @@ OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 # os.environ["https_proxy"] = st.secrets["PROXY"]
 
 # 作業ディレクトリの設定
-WORK_DIR = "static/土木技術管理規程集/砂防編_砂防"
+WORK_DIR = "static/土木技術管理規程集/河川編"
 
 # チャンクサイズとオーバーラップの設定
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 100
 
-# LangChainのDocumentクラスを保存するファイル名の設定
-DOCUMENT_PATH = "documents/土木技術管理規程集/砂防編_砂防.jsonl"
+# vectorstoreを保存するディレクトリの設定
+# 日本語のディレクトリ名はエラーになる
+VECTORSTORE_DIR = "vectorstore/faiss/kiteisyuu/kasen"
 
-# PDFをOCR処理してLangChainのDocumentクラスに変換する関数
 
+def pdf_loader(pdf_file: str) -> Iterable[Document]:
+    '''PDFをOCR処理してLangChainのDocumentに変換する関数'''
 
-def pdf_loader(pdf_file: str):
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=CHUNK_SIZE,
         chunk_overlap=CHUNK_OVERLAP,
@@ -41,30 +41,31 @@ def pdf_loader(pdf_file: str):
 
     return docs
 
-# DocumentクラスのMetaDataを加工する関数
 
+def format_metadata(org_docs: Iterable[Document], page_prefix: int) -> Iterable[Document]:
+    '''DocumentのmetaDataを加工する関数'''
 
-def format_docs(org_docs):
     docs = copy.deepcopy(org_docs)
     for doc in docs:
-        # sourceを「土木技術管理規程集_砂防編_砂防」のフォーマットに修正
+        # sourceを「土木技術管理規程集_河川編」のフォーマットに修正
         source = doc.metadata["source"].split("/")
         new_source = source[1] + "_" + source[2].split("\\")[0]
         doc.metadata.update({"source": new_source})
 
         # ページ番号を「1-1」のフォーマットに修正
-        new_page = f'{str(doc.metadata["page"]+1)}'
+        new_page = f'{str(page_prefix)}-{str(doc.metadata["page"]+1)}'
         doc.metadata.update({"page": new_page})
 
     return docs
 
-# LangChainのDocumentクラスをjsonlファイルに保存する関数
 
-
-def save_docs_to_jsonl(array: Iterable[Document], file_path: str) -> None:
-    with open(file_path, 'w') as jsonl_file:
-        for doc in array:
-            jsonl_file.write(doc.json() + '\n')
+def save_local_faiss(docs: Iterable[Document]):
+    '''DocumentをFAISSに保存する関数'''
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    db = FAISS.from_documents(docs, embeddings)
+    db.save_local(VECTORSTORE_DIR)
+    print(f'{len(docs)}個のドキュメントを{VECTORSTORE_DIR}に保存しました。')
+    return
 
 
 if __name__ == "__main__":
@@ -78,14 +79,18 @@ if __name__ == "__main__":
     for i, file in enumerate(pdf_files):
         print(f'{file}を処理中・・・')
 
+        page_prefix = file.split("_")[1].replace(".pdf", "")
+
         # PDFをOCR処理してDocumentクラスに格納
         docs = pdf_loader(file)
 
         # Documentクラスのメタデータ（出典・ページ番号）を加工
-        format = format_docs(docs)
+        formatted_docs = format_metadata(docs, i+1)
+        print(formatted_docs[0])
 
-        result.extend(format)
-        print(f'{len(format)}個のドキュメントを格納しました')
+        result.extend(formatted_docs)
+        print(f'{len(formatted_docs)}個のドキュメントを格納しました')
         print(f'ドキュメントの総数は{len(result)}個になりました。')
 
-    save_docs_to_jsonl(result, DOCUMENT_PATH)
+    # ベクトルDBに保存
+    save_local_faiss(result)
